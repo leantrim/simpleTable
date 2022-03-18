7918e6effa226b69cf285d3918ca65e2c6eed873
leantrim <leutrimbunjaku5@gmail.com>
2022-03-18 07:34:08 +0100

    created TableBody component

--- src/components/common/TableBody.jsx
+++ src/components/common/TableBody.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import styled from "styled-components";
 
 function TableBody({ columns, data }) {
+  // Generera unik id då det finns articlar i databasen med samma article_number
+  const generateUniqueKey = (item) => {
+    return item.article_number + item.price;
+  };
+
   return (
-    <>
+    <tbody>
+      {/* Mappa över data som kommer in från higher order component */}
       {data.map((item) => (
-        <Tablerow>
+        <tr key={generateUniqueKey(item)}>
+          {/* Mappa över header arrayen som kommer in från higher order component */}
           {columns.map((header) => (
-            <Td>{item[header]}</Td>
+            // skriv ut data objectet om den matchar med header
+            <Td key={item[header]}>{item[header]}</Td>
           ))}
-        </Tablerow>
+        </tr>
       ))}
-    </>
+    </tbody>
   );
 }
-
-const Tablerow = styled.tr``;
 
 const Td = styled.td`
   text-align: center;
