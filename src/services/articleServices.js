6dba63e81acff8e285294d490d52a01784a88773
leantrim <leutrimbunjaku5@gmail.com>
2022-03-18 07:33:33 +0100

    updated backend url in the frontend

--- src/services/articleServices.js
+++ src/services/articleServices.js
@@ -2,15 +2,15 @@ import axios from "axios";
 
 const getAllArticlesFromDb = async () => {
     try {
-        const { data } = await axios.get("http://localhost:5000");
+        const { data } = await axios.get('http://localhost:5000/api/articles');
         return data;
     } catch (error) {
         return error;
     }
 };
-const getArticlesFromDb = async (body) => {
+const getFilteredArticlesFromDb = async (body) => {
     try {
-        const { data } = await axios.put("http://localhost:5000", body);
+        const { data } = await axios.put('http://localhost:5000/api/articles', body);
         return data;
     } catch (error) {
         return error;
@@ -19,7 +19,7 @@ const getArticlesFromDb = async (body) => {
 
 const exportedObject = {
     getAllArticlesFromDb,
-    getArticlesFromDb
+    getFilteredArticlesFromDb
 };
 
 export default exportedObject;
